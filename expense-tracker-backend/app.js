a867babc068a1cd9bc7008f5a8c9fa1d3d936691
Ayush Ghole <ayushghole@gmail.com>
2025-02-19 23:21:33 +0530

    Update app.js

--- expense-tracker-backend/app.js
+++ expense-tracker-backend/app.js
@@ -27,5 +27,10 @@ mongoose
 app.use("/api/transactions/", require("./routes/transactionRoutes"));
 app.use("/api/budgets/", budgetRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
